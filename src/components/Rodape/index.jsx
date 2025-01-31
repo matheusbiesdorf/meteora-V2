dc3aff5859a8263f5dd4a413cdc284271d87ac2e
Matheus Biesdorf <matheusbiesdorf@hotmail.com>
2025-01-30 22:10:01 -0300

    Initial commit

---
+++ src/components/Rodape/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Rodape = () => {
+  return (
+    <footer className="text-center bg-black text-bg-dark">
+      <p className="card-text py-3">
+        2025 <i className="bi bi-c-circle"></i> Desenvolvido por Matheus Biesdorf | Projeto
+        fictício sem fins comerciais.
+      </p>
+    </footer>
+  );
+};
+
+export default Rodape;
